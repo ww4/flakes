a3c2a599a4d89027884258cd42fd776fa04aa448
Chris <chris@saenzmail.net>
2023-11-10 15:24:34 -0500

    updated jellyfin

--- configuration.nix
+++ configuration.nix
@@ -264,8 +264,21 @@
   services.openssh.enable = true;
 
 # Jellyfin
-  services.jellyfin.enable = true;
+  services.jellyfin = {
+    enable = true;
+    package = pkgs.jellyfin.override {
+      jellyfin-web = pkgs.jellyfin-web.overrideAttrs (oldAttrs: {
+        patches = [
+          (pkgs.fetchpatch {
+            url = "https://github.com/jellyfin/jellyfin-web/compare/v${oldAttrs.version}...ConfusedPolarBear:jellyfin-web:intros.diff";
+            hash = "sha256-qm4N4wMUFc4I53oQJUK1Six0cahVYz3J+FgO2vvSvXM=";
+          })
+        ];
+      });
+    };
+  };
 
+  
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
