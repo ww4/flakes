cfe124174cee6822af294031febf144510649b11
Chris <chris@saenzmail.net>
2023-11-17 16:51:14 -0500

    Nextcloud is working! Still need to move datadir

--- nextcloud.nix
+++ nextcloud.nix
@@ -1,28 +1,28 @@
 {config, pkgs, ...}:
 {
-#services.nginx = {
-#   enable = true;
-#
-# # Use recommended settings
-#    recommendedGzipSettings = true;
-#    recommendedOptimisation = true;
-#    recommendedProxySettings = true;
-#    recommendedTlsSettings = true;
-#
-# # Only allow PFS-enabled ciphers with AES256
-# #   sslCiphers = "AES256+EECDH:AES256+EDH:!aNULL";
-#
-## Setup Nextcloud virtual host to listen on ports
-# virtualHosts = {
-#
-#     "cloud.rosemaryacres.com" = {
-#       ## Force HTTP redirect to HTTPS
-#        forceSSL = true;
-#       ## LetsEncrypt
-#       # enableACME = true;
+# services.nginx = {
+#    enable = true;
+# #
+# # # Use recommended settings
+# #    recommendedGzipSettings = true;
+# #    recommendedOptimisation = true;
+# #    recommendedProxySettings = true;
+# #    recommendedTlsSettings = true;
+# #
+# # # Only allow PFS-enabled ciphers with AES256
+# # #   sslCiphers = "AES256+EECDH:AES256+EDH:!aNULL";
+# #
+# ## Setup Nextcloud virtual host to listen on ports
+#   virtualHosts = {
+# #
+#       "cloud.rosemaryacres.com" = {
+# #       ## Force HTTP redirect to HTTPS
+# #        forceSSL = true;
+# #       ## LetsEncrypt
+# #       # enableACME = true;
+#      };
 #    };
-#  };
-#};
+# };
 services.nextcloud = {
     enable = true;
     hostName = "cloud.rosemaryacres.com";
@@ -33,7 +33,7 @@ services.nextcloud = {
     autoUpdateApps.enable = true;
     # Set what time makes sense for you
     autoUpdateApps.startAt = "05:00:00";
-
+ #  datadir = "/mnt/fusion/nextcloud"; # Temporarily disabled to track permissions issues 
     config = {
       # Further forces Nextcloud to use HTTPS
       # overwriteProtocol = "https";
@@ -42,9 +42,7 @@ services.nextcloud = {
       dbtype = "pgsql";
       dbuser = "nextcloud";
       dbhost = "/run/postgresql"; # nextcloud will add /.s.PGSQL.5432 by itself
-      dbname = "nextcloud";
-      dbpassFile = "/var/nextcloud-db-pass";
-
+      dbname = "nextcloud-db";
       adminpassFile = "/var/nextcloud-admin-pass";
       adminuser = "admin";
  };
@@ -53,10 +51,10 @@ services.postgresql = {
     enable = true;
 
     # Ensure the database, user, and permissions always exist
-    ensureDatabases = [ "nextcloud" ];
+    ensureDatabases = [ "nextcloud-db" ];
     ensureUsers = [
      { name = "nextcloud";
-       ensurePermissions."DATABASE nextcloud" = "ALL PRIVILEGES";
+       ensurePermissions."DATABASE \"nextcloud-db\"" = "ALL PRIVILEGES";
      }
     ];
 };
