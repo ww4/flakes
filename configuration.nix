f8a0aa798ec986ca0294c5775e219fddf24cd2f3
Chris <chris@saenzmail.net>
2024-11-09 23:30:18 -0500

    Restructuring mergerfs into 3 buckets

--- configuration.nix
+++ configuration.nix
@@ -49,7 +49,7 @@
 
   fileSystems = {
   # Physical drives
-      "/mnt/mergerD1" = { 
+      "/mnt/decom/D1" = {  # was mergerD1
         device = "/dev/disk/by-uuid/4731e760-fe51-48ef-8e35-5b764b84c249";
         fsType = "ext4";
         options = [
@@ -57,7 +57,7 @@
         ];
       };
 
-      "/mnt/mergerD2" = {
+      "/mnt/decom/D2" = {  # was mergerD2
         device = "/dev/disk/by-uuid/d49dac75-4d28-4c56-b2d2-606b3271b9b5";
         fsType = "ext4";
         options = [
@@ -65,7 +65,7 @@
         ];
       };
 
-      "/mnt/mergerD3" = {
+      "/mnt/decom/D3" = {  # was mergerD3
         device = "/dev/disk/by-uuid/a500d06c-9878-4db3-9873-d0ee7f07dfc0";
         fsType = "ext4";
         options = [
@@ -73,23 +73,15 @@
         ];
       };
 
-      "/mnt/mergerD4" = {
+      "/mnt/decom/D4" = { # was mergerD4
         device = "/dev/disk/by-uuid/f11eeedc-60aa-4567-948a-c19ff0ccf337";
         fsType = "ext4";
         options = [
           "nofail"
         ];
       };
-      
-#      "/mnt/mergerD5" = {
-#        device = "/dev/disk/by-uuid/20b0b177-ab99-45e3-b4fe-1d06e6ce43ca";
-#        fsType = "ext4";
-#        options = [
-#          "nofail"
-#        ];
-#      };
 
-      "/mnt/mergerD6" = {
+      "/mnt/backup/D1" = { # was mergerD6
         device = "/dev/disk/by-uuid/c9e23776-811b-4dec-9fc5-3e55454a21f0";
         fsType = "ext4";
         options = [
@@ -97,7 +89,7 @@
         ];
       };
 
-      "/mnt/mergerD7" = {
+      "/mnt/backup/D2" = { # was mergerD7
         device = "/dev/disk/by-uuid/5411719d-176a-4e59-8ec2-c4efef9d5410";
         fsType = "ext4";
         options = [
@@ -105,7 +97,7 @@
         ];
       };
 
-      "/mnt/mergerD8" = {
+      "/mnt/backup/D3" = { # was mergerD8
         device = "/dev/disk/by-uuid/4b986746-3de1-49de-8c02-850adcb9024e";
         fsType = "ext4";
         options = [
@@ -113,7 +105,7 @@
         ];
       };
 
-      "/mnt/mergerD9" = {
+      "/mnt/backup/D4" = { # was mergerD9
         device = "/dev/disk/by-uuid/11841ae6-181e-4345-ab89-bea16dcdcc59";
         fsType = "ext4";
         options = [
@@ -121,7 +113,7 @@
         ];
       };
 
-       "/mnt/justD10" = {
+       "/mnt/primary/D1" = { #was mergerD10 / Superbook
         device = "/dev/disk/by-uuid/8976631f-34b3-4192-b979-4012a66f233f";
         fsType = "xfs";
         options = [
@@ -129,23 +121,53 @@
         ];
       };
 
-#       "/mnt/mergerD11" = {
-#        device = "/dev/disk/by-uuid/903A27FB3A27DD4A";
-#        fsType = "ntfs";
-#        options = [
-#          "nofail"
-#        ];
-#      };
+       "/mnt/primary/D2" = { # was mergerD11
+        device = "/dev/disk/by-uuid/903A27FB3A27DD4A";
+        fsType = "ntfs";
+        options = [
+          "nofail"
+        ];
+      };
 
        "/home/chris" = {
         device = "/dev/disk/by-uuid/56c90b01-5f1e-4058-a2c4-c3db4df4deef";
         fsType = "ext4";
       };
   
-  # mergerfs: merge drives
+  # mergerfs: using 3 buckets, rsynced, then remove #3 for archival storage.
   
-    "/mnt/fusion" = {
-      device = "/mnt/mergerD*";
+    "/mnt/fusion" = {   # Primary Bucket - 16.4 TB (contains 8TB + 10TB drive)
+      device = "/mnt/primary/D*";
+      fsType = "fuse.mergerfs";
+      options = [
+        "defaults"
+        "allow_other"
+        "use_ino"
+        "cache.files=off"
+        "moveonenospc=true"
+        "dropcacheonclose=true"
+        "category.create=mfs"
+        "nofail"
+      ];
+    };  
+
+     "/mnt/backup/all" = {   # Backup Bucket - 22 TB (contains 4x 6TB drives)
+      device = "/mnt/backup/D*";
+      fsType = "fuse.mergerfs";
+      options = [
+        "defaults"
+        "allow_other"
+        "use_ino"
+        "cache.files=off"
+        "moveonenospc=true"
+        "dropcacheonclose=true"
+        "category.create=mfs"
+        "nofail"
+      ];
+    };  
+
+     "/mnt/decom/all" = {   # Decom Bucket - 10.8 TB of soon-to-be decommisioned drives. Remove this group once done.
+      device = "/mnt/decom/D*";
       fsType = "fuse.mergerfs";
       options = [
         "defaults"
